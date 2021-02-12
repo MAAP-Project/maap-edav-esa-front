995b97419714f085201584674de2916cac98b50e
stefano marra <stefano.marra@cgi.com>
2021-02-12 11:10:31 +0100

    fix react-dnd dependency issue

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { DndProvider } from 'react-dnd';
-import HTML5Backend from 'react-dnd-html5-backend';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
 import axios from 'axios';
 
